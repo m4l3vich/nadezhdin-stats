a63aef6e959f7bb26586ab86d2575f4d75ca2f8d
Evgeny Malevich <m4l3vich@mail.ru>
2024-01-24 05:12:26 +0800

    feat: PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxtjs/eslint-module', ['@nuxtjs/google-fonts', {
     families: { Inter: { wght: [400, 500, 600, 700] } },
     subsets: ['cyrillic']
@@ -8,5 +9,5 @@ export default defineNuxtConfig({
     defaultImport: 'component'
   }], 'nuxt-icon', ['yandex-metrika-module-nuxt3', {
     id: 96222063
-  }]]
+  }], '@vite-pwa/nuxt']
 })
